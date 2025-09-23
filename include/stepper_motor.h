5b3da2c719823fb13c2013f510b965016a401992
JimInput <oJimmy05o@gmail.com>
2025-09-23 02:14:55 -0400

    fixed tabbing

--- include/stepper_motor.h
+++ include/stepper_motor.h
@@ -4,16 +4,16 @@
 
 class StepperMotor {
 public:
-	StepperMotor(int steps_per_rev, int step_pin, int dir_pin);
-	void set_speed(float sps);
-	void run(float dt);
+    StepperMotor(int steps_per_rev, int step_pin, int dir_pin);
+    void set_speed(float sps);
+    void run(float dt);
 
 private:
-	int steps_per_rev_;
-	int step_pin_;
-	int dir_pin_;
-	
-	float speed_sps_;
-	float frac_steps_;
-	int step_pulse_high_us_;
+    int steps_per_rev_;
+    int step_pin_;
+    int dir_pin_;
+    
+    float speed_sps_;
+    float frac_steps_;
+    int step_pulse_high_us_;
 };
--- src/stepper_motor.cpp
+++ src/stepper_motor.cpp
@@ -3,42 +3,42 @@
 #include "../include/stepper_motor.h"
 
 StepperMotor::StepperMotor(int steps_per_rev, int step_pin, int dir_pin) {
-	steps_per_rev_ = steps_per_rev;
-	step_pin_ = step_pin;
-	dir_pin_ = dir_pin;
+    steps_per_rev_ = steps_per_rev;
+    step_pin_ = step_pin;
+    dir_pin_ = dir_pin;
 
-	speed_sps_ = 0.0f;
-	frac_steps_ = 0.0f;
-	step_pulse_high_us_ = 4;
+    speed_sps_ = 0.0f;
+    frac_steps_ = 0.0f;
+    step_pulse_high_us_ = 4;
 
-	pinMode(step_pin_, OUTPUT);
-	pinMode(dir_pin_, OUTPUT);
-	digitalWrite(step_pin_, LOW);
+    pinMode(step_pin_, OUTPUT);
+    pinMode(dir_pin_, OUTPUT);
+    digitalWrite(step_pin_, LOW);
 }
 
 void StepperMotor::set_speed(float sps) {
-	speed_sps_ = sps;
+    speed_sps_ = sps;
 }
 
 void StepperMotor::run(float dt) {
-	if (speed_sps_ <= 0.0f || dt <= 0) return;
+    if (speed_sps_ <= 0.0f || dt <= 0) return;
 
-	float steps_due = speed_sps_ * dt + frac_steps_;
-	int steps_now = static_cast<int>(steps_due);
-	frac_steps_ = steps_due - static_cast<float>(steps_now);
+    float steps_due = speed_sps_ * dt + frac_steps_;
+    int steps_now = static_cast<int>(steps_due);
+    frac_steps_ = steps_due - static_cast<float>(steps_now);
 
-	const int MAX_PULSES_PER_CALL = 8;
-	int emit = steps_due;
-	if (emit > MAX_PULSES_PER_CALL) {
-		frac_steps_ += static_cast<float>(emit - MAX_PULSES_PER_CALL);
-		emit = MAX_PULSES_PER_CALL;
-	}
+    const int MAX_PULSES_PER_CALL = 8;
+    int emit = steps_due;
+    if (emit > MAX_PULSES_PER_CALL) {
+        frac_steps_ += static_cast<float>(emit - MAX_PULSES_PER_CALL);
+        emit = MAX_PULSES_PER_CALL;
+    }
 
-	for (int i = 0; i < emit; ++i) {
-		digitalWrite(step_pin_, HIGH);
-		delayMicroseconds(step_pulse_high_us_);
-		digitalWrite(step_pin_, LOW);
-		delayMicroseconds(step_pulse_high_us_);
-	}
+    for (int i = 0; i < emit; ++i) {
+        digitalWrite(step_pin_, HIGH);
+        delayMicroseconds(step_pulse_high_us_);
+        digitalWrite(step_pin_, LOW);
+        delayMicroseconds(step_pulse_high_us_);
+    }
 }
 
